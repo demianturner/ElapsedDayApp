04f5cd3a5f30c6618e1ccbae18a36bd466cd089e
Demian Turner <demian@muse23.com>
2025-06-13 13:31:58 +0100

    Initial Commit

---
+++ ElapsedDayApp/ElapsedDayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElapsedDayAppApp.swift
+//  ElapsedDayApp
+//
+//  Created by Demian Turner on 13/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ElapsedDayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
